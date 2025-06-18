352942bd2f426e710533c68b1215ae320aedf439
Gezim5 <gezimcelaa@outlook.com>
2025-06-19 00:50:46 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js (at project root)
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/', // or '/ardi-bau/' if you're hosting under a sub-path
-  plugins: [react()],
-});
